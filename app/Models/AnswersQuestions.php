86c982d28f4025ea89d769277047f3b411ff19f7
denchiklut <denchiklut@bitbucket.org>
2019-02-12 12:17:41 +0300

    Add 2 models

---
+++ app/Models/AnswersQuestions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AnswersQuestions extends Model
+{
+    protected $table = 'answers_questions';
+    protected $guarded = ['id'];
+}
---
+++ app/Models/QuestionsAnswers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionsAnswers extends Model
+{
+    protected $table = 'questions_answers';
+    protected $guarded = ['id'];
+}
