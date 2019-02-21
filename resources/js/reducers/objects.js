bf4042a60013f72eec8efd3665eb1165d6aeef9a
denchiklut <denchiklut@bitbucket.org>
2019-02-21 20:11:39 +0300

    Добавил изменение обьектов

--- resources/js/reducers/objects.js
+++ resources/js/reducers/objects.js
@@ -5,7 +5,7 @@ export default function (state = [], action = {}) {
         case SET_OBJECTS:   return action.payload
         case ADD_OBJECT:    return state.concat(action.payload)
         case DELETE_OBJECT: return state.filter((item) => item._id !== action.payload._id)
-        case UPDATE_OBJECT: return state.map((object) => object._id === action.payload._id ? {...object, value:action.payload.value}:object)
+        case UPDATE_OBJECT: return state.map((object) => object._id === action.payload._id ? {...object, value:action.payload.value, name:action.payload.name}:object)
 
         default:  return state
     }
