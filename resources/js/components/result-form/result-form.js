1df753abc663352910af5d62895ad087655e2488
denchiklut <denchiklut@bitbucket.org>
2019-03-05 18:06:38 +0300

    Some bug fixed Закончил вывод проекта. Все в одной куче из-за сроков. НУЖЕН рефакторинг КОДА

--- resources/js/components/result-form/result-form.js
+++ resources/js/components/result-form/result-form.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import './result-form.scss'
 import {bindActionCreators} from "redux";
 import {fetchNodes} from "../../actions/graf/nodes";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Fab from "@material-ui/core/Fab";
+import Button from "@material-ui/core/Button";
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import './result-form.scss'
 
+function Transition(props) {
+    return <Slide direction="right" {...props} />;
+}
 
 class ResultForm extends Component {
 
     state = {
         questionList: [],
         currentChild: null,
+        open: false,
     }
 
     findNodes = (answer, newData) => {
@@ -26,7 +43,7 @@ class ResultForm extends Component {
                     }
 
                     this.setState(prevState => ({
-                        questionList: [ ...prevState.questionList, {question: item.name, answers: answers}],
+                        questionList: [ ...prevState.questionList, {...item, answers: answers}],
                         currentChild: { ...item }
                     }))
                 }
@@ -49,13 +66,75 @@ class ResultForm extends Component {
         this.findNodes("start", newData)
     }
 
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
     render() {
         return (
             <div className="ResultForm">
-                {this.state.questionList !==0 ?
+                {this.state.questionList !== 0 ?
                     this.state.questionList.map(item =>
-                        <div key={item}>
-                            <p>{item.question}</p>
+                        <div key={item.idd}>
+
+
+                            {item.type === "objects" ?
+                                <div>
+                                <Table className="resTable">
+                                    <TableHead style={{background: '#d8d8d8'}}>
+                                        <TableRow>
+                                        <TableCell>Название</TableCell>
+                                        <TableCell align="right">Расположение</TableCell>
+                                        <TableCell align="right">Цена</TableCell>
+                                        <TableCell align="right">Срок сдачи</TableCell>
+                                        <TableCell align="right">Отделка</TableCell>
+                                        <TableCell align="right">Вид</TableCell>
+                                    </TableRow>
+                                    </TableHead>
+                                <TableBody>
+                                    <TableRow key={item.objData.name}>
+                                        <TableCell component="th" scope="row">
+                                            <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+                                                {item.objData.name}
+                                            </Button>
+
+                                        </TableCell>
+                                        <TableCell align="right">{item.objData.location}</TableCell>
+                                        <TableCell align="right">{item.objData.cost}</TableCell>
+                                        <TableCell align="right">{item.objData.time}</TableCell>
+                                        <TableCell align="right">{item.objData.otdelka}</TableCell>
+                                        <TableCell align="right">{item.objData.view}</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                                </Table>
+
+                                    <Dialog
+                                        fullScreen
+                                        open={this.state.open}
+                                        onClose={this.handleClose}
+                                        TransitionComponent={Transition}
+                                    >
+                                        <AppBar position="static" >
+                                            <Toolbar>
+                                                <IconButton color="inherit" onClick={this.handleClose} aria-label="Close">
+                                                    <CloseIcon />
+                                                </IconButton>
+                                            </Toolbar>
+                                        </AppBar>
+                                        <Typography variant="div" color="inherit" >
+                                            <pre style={{ whiteSpace: 'pre-wrap', margin: '15px'}}>
+                                                {item.objData.dopInformation}
+                                            </pre>
+                                        </Typography>
+                                    </Dialog>
+                                </div>
+
+                                 : <p key={item.idd}>{item.name}</p>
+                            }
 
                             {item.answers.map(item =>
                                 <Fab
