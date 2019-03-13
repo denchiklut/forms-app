9b45e9ce064c0e69894fea5aaad52e529c4e190c
denchiklut <aleksandrov@reffection.com>
2019-03-14 00:16:34 +0300

    Changed OAuth Google

--- resources/js/components/header-bar/header-bar.js
+++ resources/js/components/header-bar/header-bar.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from '@material-ui/icons/Menu';
-import Typography from "@material-ui/core/Typography";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import React from 'react'
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from '@material-ui/icons/Menu'
+import Typography from "@material-ui/core/Typography"
+import CssBaseline from "@material-ui/core/CssBaseline"
 import SelectProject from '../select-project'
 import { Link } from 'react-router-dom'
 import GoogleAuth from '../google-auth'
-import Hidden from '@material-ui/core/Hidden';
+import Avatar from '@material-ui/core/Avatar'
+import Hidden from '@material-ui/core/Hidden'
+import {connect} from 'react-redux';
 import './header-bar.scss'
 
 
@@ -26,8 +28,13 @@ const HeaderBar = props => (
                     aria-label="Open drawer"
                     className="menu-btn"
                 >
-                    <Link to="/" style={{textDecoration: "none", color: "white"}}>
-                        <MenuIcon />
+                    <Link to="/" style={{textDecoration: "none", color: "white", fontSize: 0}}>
+                        {props.profile.avatar ?
+                            <Avatar
+                                alt={props.profile.name}
+                                src={props.profile.avatar}
+                            /> :
+                            <MenuIcon />}
                     </Link>
                 </IconButton>
                 <Hidden smDown>
@@ -39,7 +46,7 @@ const HeaderBar = props => (
                         className="menu-title"
                     >
                         <Link to="/" style={{textDecoration: "none", color: "white"}}>
-                            Editor
+                            {props.profile.name ? props.profile.name : 'Editor' }
                         </Link>
                     </Typography>
                 </Hidden>
@@ -55,4 +62,10 @@ const HeaderBar = props => (
     </div>
 );
 
-export default HeaderBar;
+const mapStateToProps = (state) => {
+    return {
+        profile: state.auth
+    }
+}
+
+export default connect(mapStateToProps)(HeaderBar);
