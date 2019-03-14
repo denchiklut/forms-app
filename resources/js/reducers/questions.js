d4c8d863448dc8de526b721c7cce80ef3802ec50
denchiklut <aleksandrov@reffection.com>
2019-03-14 23:40:46 +0300

    Поправил баг с изменением вопроса

--- resources/js/reducers/questions.js
+++ resources/js/reducers/questions.js
@@ -5,7 +5,7 @@ export default function (state = [], action = {}) {
         case SET_QUESTIONS:   return action.payload
         case ADD_QUESTION:    return state.concat(action.payload)
         case DELETE_QUESTION: return state.filter((item) => item._id !== action.payload._id)
-        case UPDATE_QUESTION: return state.map((question) => question._id === action.payload._id ? {...question, name:action.payload.name}:question)
+        case UPDATE_QUESTION: return state.map((question) => question._id === action.payload._id ? {...question, name: action.payload.name, webName: action.payload.webName}:question)
 
         default:  return state
     }
