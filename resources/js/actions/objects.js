d8cc62a56be616370e402d83a34de58ea69e75e7
denchiklut <denchiklut@bitbucket.org>
2019-03-18 11:23:17 +0300

    Fixed Objects actions

--- resources/js/actions/objects.js
+++ resources/js/actions/objects.js
@@ -6,79 +6,32 @@ export const ADD_OBJECT = "ADD_OBJECT"
 export const DELETE_OBJECT = "DELETE_OBJECT"
 export const UPDATE_OBJECT = "UPDATE_OBJECT"
 
-export function setObjects( objects ) {
-    return {
-        type: SET_OBJECTS,
-        payload: objects
-    }
-}
-
-export const selectObject = ( object ) => {
-    return {
-        type: OBJECT_SELECTED,
-        payload: object
-    }
-}
-
-export const addObject = (object) => {
-    return {
-        type: ADD_OBJECT,
-        payload: object
-    }
-}
-
-export const deleteObject = ( delObject ) => {
-    return {
-        type: DELETE_OBJECT,
-        payload: delObject
-    }
-}
-
-export const updateObject = ( editObject ) => {
-    return {
-        type: UPDATE_OBJECT,
-        payload: editObject
-    }
-}
-
-export function fetchObjects () {
-    return dispatch => {
-        axios
-            .get(`/api/objects/`)
-            .then(data => dispatch (setObjects(data.data)))
-            .catch(( err ) => console.log ( err ))
-
-    }
+export const selectObject = object => {
+    return { type: OBJECT_SELECTED, payload: object }
 }
 
 
-export function fetchAddObject ( newObject ) {
-    return dispatch => {
-        axios
-            .post('/api/objects', newObject)
-            .then(res => dispatch ( addObject( res.data ) ))
-            .catch((err ) => console.log ( err ))
-
-    }
+export const fetchObjects = () => async dispatch => {
+    const result = await axios.get(`/api/objects/`)
+    dispatch({ type: SET_OBJECTS, payload: result.data })
 }
 
-export function fetchDeleteObject ( delObject ) {
-    return dispatch => {
-        axios
-            .delete(`/api/objects/${delObject._id}`)
-            .then(res => dispatch(deleteObject( delObject) ))
-            .catch((err)=>console.log(err))
 
-    }
+
+export const fetchAddObject = newObject => async dispatch => {
+    const result = await axios.post('/api/objects', newObject)
+    dispatch({ type: ADD_OBJECT, payload: result.data })
 }
 
-export function fetchUpdateObject ( object ) {
-    return dispatch => {
-        axios
-            .patch(`/api/objects/${object._id}`, object)
-            .then(res => dispatch(updateObject(object)))
-            .catch((err)=>console.log(err))
 
-    }
+export const fetchDeleteObject = delObject => async dispatch => {
+    await axios.delete(`/api/objects/${delObject._id}`)
+    dispatch({ type: DELETE_OBJECT, payload: delObject })
+}
+
+
+export const fetchUpdateObject = object => async dispatch =>{
+    await axios.patch(`/api/objects/${object._id}`, object)
+    dispatch({ type: UPDATE_OBJECT, payload: object })
 }
 
