4f38a429b79cadd58f79a0fa2161fa87fd66a6e1
denchiklut <denchiklut@bitbucket.org>
2019-03-18 11:43:18 +0300

    Fixed Nodes actions

--- resources/js/actions/graf/nodes.js
+++ resources/js/actions/graf/nodes.js
@@ -4,55 +4,20 @@ export const ADD_NODE ="ADD_NODE"
 export const REMOVE_NODE ="REMOVE_NODE"
 
 
-export function setNodes(nodes) {
-    return {
-        type: SET_NODES,
-        payload: nodes
-    }
+export const fetchNodes = project => async dispatch => {
+    const result = axios.get(`/api/nodes/${project}`)
+    dispatch({ type: SET_NODES, payload: result.data })
 }
 
-export function addNode(node, graf) {
-    return {
-        type: ADD_NODE,
-        addedNode: node,
-        payload: graf
-    }
+
+export const onAddNode = ( node, graf ) => async dispatch => {
+    const result = await axios.post('/api/nodes', {data: graf})
+    dispatch({ type: ADD_NODE, addedNode: node, payload: result.data })
 }
 
-export function removeNode(node, graf) {
-    return {
-        type: REMOVE_NODE,
-        removedNode: node,
-        payload: graf
-    }
-}
 
-export function fetchNodes(project) {
-    return dispatch => {
-        axios
-            .get(`/api/nodes/${project}`)
-            .then(data => dispatch(setNodes(data.data)))
-            .catch((err)=>console.log(err))
+export const onRemoveNode = ( node, graf ) => async dispatch => {
+    const result = await axios.post('/api/nodes', {data: graf})
+    dispatch({ type: REMOVE_NODE, removedNode: node, payload: result.data })
 
-    }
-}
-
-export function onAddNode( node, graf ) {
-    return dispatch => {
-        axios
-            .post('/api/nodes', {data: graf})
-            .then(res => dispatch(addNode(node, res.data)))
-            .catch((err)=>console.log(err))
-
-    }
-}
-
-export function onRemoveNode( node, graf ) {
-    return dispatch => {
-        axios
-            .post('/api/nodes', {data: graf})
-            .then(res => dispatch(removeNode(node, res.data)))
-            .catch((err)=>console.log(err))
-
-    }
 }
