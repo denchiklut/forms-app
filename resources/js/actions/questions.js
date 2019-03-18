bf7d48c5cbe0382773594fca99328fbbc5c87f14
denchiklut <denchiklut@bitbucket.org>
2019-03-18 11:33:06 +0300

    Fixed Questions actions

--- resources/js/actions/questions.js
+++ resources/js/actions/questions.js
@@ -6,79 +6,33 @@ export const ADD_QUESTION = "ADD_QUESTION"
 export const DELETE_QUESTION = "DELETE_QUESTION"
 export const UPDATE_QUESTION = "UPDATE_QUESTION"
 
-export function setQuestions(questions) {
-    return {
-        type: SET_QUESTIONS,
-        payload: questions
-    }
-}
-
 export const selectQuestion = (question) => {
-   return {
+    return {
         type: QUESTION_SELECTED,
         payload: question
     }
 }
 
-export const addQuestion = (question) => {
-    return {
-        type: ADD_QUESTION,
-        payload: question
-    }
-}
-
-export const deleteQuestion = (delQuestion) => {
-    return {
-        type: DELETE_QUESTION,
-        payload: delQuestion
-    }
-}
-
-export const updateQuestion = (editQuestion) => {
-    return {
-        type: UPDATE_QUESTION,
-        payload: editQuestion
-    }
-}
-
-export function fetchQuestions(project) {
-    return dispatch => {
-        axios
-            .get(`/api/questions/${project}`)
-            .then(data => dispatch(setQuestions(data.data)))
-            .catch((err)=>console.log(err))
-
-    }
+export const fetchQuestions = project => async dispatch =>{
+    const result = await axios.get(`/api/questions/${project}`)
+    dispatch({ type: SET_QUESTIONS, payload: result.data })
 }
 
 
-export function fetchAddQuestion(newQuestion) {
-    return dispatch => {
-        axios
-            .post('/api/questions', newQuestion)
-            .then(res => dispatch(addQuestion(res.data)))
-            .catch((err)=>console.log(err))
-
-    }
+export const fetchAddQuestion = newQuestion => async dispatch => {
+    const result = await axios.post('/api/questions', newQuestion)
+    dispatch({ type: ADD_QUESTION, payload: result.data })
 }
 
-export function fetchDeleteQuestion(delQuestion) {
-    return dispatch => {
-        axios
-            .delete(`/api/questions/${delQuestion._id}`)
-            .then(res => dispatch(deleteQuestion(delQuestion)))
-            .catch((err)=>console.log(err))
 
-    }
+export const fetchDeleteQuestion = delQuestion => async dispatch => {
+    await axios.delete(`/api/questions/${delQuestion._id}`)
+    dispatch({ type: DELETE_QUESTION, payload: delQuestion })
 }
 
-export function fetchUpdateQuestion(question) {
-    return dispatch => {
-        axios
-            .patch(`/api/questions/${question._id}`, question)
-            .then(res => dispatch(updateQuestion(question)))
-            .catch((err)=>console.log(err))
 
-    }
+export const fetchUpdateQuestion = question => async dispatch => {
+    await axios.patch(`/api/questions/${question._id}`, question)
+    dispatch({ type: UPDATE_QUESTION, payload: editQuestion })
 }
 
