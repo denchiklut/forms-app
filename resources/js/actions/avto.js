5f05e65975ee7bd8794dbbe74f220c86966ed5e2
denchiklut <denchiklut@bitbucket.org>
2019-03-22 17:34:23 +0300

    Add avto action

--- resources/js/actions/avto.js
+++ resources/js/actions/avto.js
@@ -9,3 +9,22 @@ export const fetchAvto = () => async dispatch => {
     const result = await axios.get(`/api/avto/`)
     dispatch({ type: SET_AVTO, payload: result.data })
 }
+
+export const fetchAddObject = newAvto => async dispatch => {
+    const result = await axios.post('/api/avto', newAvto)
+    dispatch({ type: ADD_AVTO, payload: result.data })
+}
+
+
+export const fetchDeleteObject = delAvto => async dispatch => {
+    await axios.delete(`/api/avto/${delAvto._id}`)
+    dispatch({ type: DELETE_AVTO, payload: delAvto })
+}
+
+
+export const fetchUpdateObject = avto => async dispatch =>{
+    await axios.patch(`/api/avto/${avto._id}`, avto)
+    dispatch({ type: UPDATE_AVTO, payload: avto })
+}
+
+
