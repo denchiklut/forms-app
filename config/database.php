522d56f29440c777cd0dc969cb63bec83c42c0fc
denchiklut <denchiklut@bitbucket.org>
2019-03-25 15:46:52 +0300

    Mongo

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
             'options'  => [
-                'database' => env('DB_DATABASE') // sets the authentication database required by mongo 3
+                'database' => 'admin' // sets the authentication database required by mongo 3
             ]
         ],
 
