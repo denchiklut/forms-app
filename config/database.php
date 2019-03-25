73d635f09f7e18580272209bfbba40e910032cbe
denchiklut <denchiklut@bitbucket.org>
2019-03-25 13:30:40 +0300

    Mongo

--- config/database.php
+++ config/database.php
@@ -40,9 +40,9 @@ return [
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
-            'options'  => array(
+            'options'  => [
                 'database' => env('DB_DATABASE') // sets the authentication database required by mongo 3
-            )
+            ]
         ],
 
         'sqlite' => [
--- docker_build.sh
+++ docker_build.sh
@@ -3,6 +3,6 @@ docker stop app-forms
 docker rm app-forms
 docker rmi app-forms
 docker build -t app-forms .
-docker run -d --name app-forms -p 127.0.0.1:8001:80 --restart=always app-forms
+docker run -d --name app-forms -p 127.0.0.1:8001:80 --restart=always --network app-forms app-forms
 sleep 5
 docker ps -a
