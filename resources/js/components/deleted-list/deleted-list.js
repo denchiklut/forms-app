2c861f66ec3f67d23d42f74ae1e5de8ab890fb44
denchiklut <denchiklut@bitbucket.org>
2019-04-29 11:43:37 +0300

    Add search to deleted list

--- resources/js/components/deleted-list/deleted-list.js
+++ resources/js/components/deleted-list/deleted-list.js
@@ -1,20 +1,57 @@
-import React, {Component} from 'react';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
+import React, {Component} from 'react'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItem from '@material-ui/core/ListItem'
+import List from '@material-ui/core/List'
 import classNames from 'classnames'
+import Toolbar from "@material-ui/core/Toolbar"
+import SearchIcon from '@material-ui/icons/Search'
+import InputBase from "@material-ui/core/InputBase"
+import AppBar from "@material-ui/core/AppBar"
+import './deleted-list.scss'
+
 
 class DeletedList extends Component {
+
+    state = {
+        term: ''
+    }
 
     handleListItemClick = (event, index, item) => {
         this.props.onSelect({...item, type: this.props.type})
     };
 
+    filterFun = (item) => {
+        let result =  item.name.toLowerCase().indexOf(this.state.term.toLowerCase()) !== -1
+
+        if (!result) {
+            result =  item._id.toLowerCase().indexOf(this.state.term.toLowerCase()) !== -1
+        }
+
+        return result
+    }
+
     render() {
+        const filtered = this.props.items.filter((item) => this.filterFun(item))
         return (
-            <div>
+            <div className="deletedList">
                 <List component="nav" className="backupList">
-                    {this.props.items.map((item, idx) => (
+                    <AppBar position="static">
+                        <Toolbar className="searchWrp">
+                            <div className="searchCnt">
+                                <div className="searchIcn">
+                                    <SearchIcon />
+                                </div>
+                                <InputBase
+                                    className="searchInp"
+                                    placeholder="Search…"
+                                    value={this.state.term}
+                                    onChange={(e) => this.setState({term: e.target.value})}
+                                />
+                            </div>
+                        </Toolbar>
+                    </AppBar>
+
+                    {filtered.map((item, idx) => (
                         <ListItem
                             button
                             key={item._id}
